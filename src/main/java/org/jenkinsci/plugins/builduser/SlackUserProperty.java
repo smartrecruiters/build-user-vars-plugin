0ed01fc87ab2d0d2d3952bc28428b8aacdcddda1
Michal Slusarczyk <m.slusarczyk@smartrecruiters.com>
2018-11-28 15:49:48 +0100

    Update SlackUserProperty.java

--- src/main/java/org/jenkinsci/plugins/builduser/SlackUserProperty.java
+++ src/main/java/org/jenkinsci/plugins/builduser/SlackUserProperty.java
@@ -43,7 +43,7 @@ public class SlackUserProperty extends UserProperty {
         if (nonNullSlackUsername.isEmpty()) {
             return nonNullSlackUsername;
         }
-        return format("@<%s>", nonNullSlackUsername);
+        return format("<@%s>", nonNullSlackUsername);
     }
 
     @Extension
