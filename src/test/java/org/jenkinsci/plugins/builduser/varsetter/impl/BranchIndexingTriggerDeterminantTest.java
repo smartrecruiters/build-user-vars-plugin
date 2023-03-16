72efbff1c082769b030b10b910735bbc8cbd84ff
spotoczny <s.potoczny@smartrecruiters.com>
2023-03-16 12:15:56 +0100

    chore: Fix test cases and add smartrecruiters custom plugin repository.

--- src/test/java/org/jenkinsci/plugins/builduser/varsetter/impl/BranchIndexingTriggerDeterminantTest.java
+++ src/test/java/org/jenkinsci/plugins/builduser/varsetter/impl/BranchIndexingTriggerDeterminantTest.java
@@ -1,7 +1,6 @@
 package org.jenkinsci.plugins.builduser.varsetter.impl;
 
 import java.io.IOException;
-import java.lang.reflect.Constructor;
 import java.util.Collections;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -11,17 +10,13 @@ import hudson.model.Run;
 import hudson.model.TaskListener;
 import hudson.util.LogTaskListener;
 import jenkins.branch.BranchIndexingCause;
-import org.jenkinsci.plugins.builduser.varsetter.IUsernameSettable;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasEntry;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 public class BranchIndexingTriggerDeterminantTest {
     @Rule
@@ -39,24 +34,5 @@ public class BranchIndexingTriggerDeterminantTest {
         Run run = r.createFreeStyleProject().getBuild("0");
         assertFalse(new BranchIndexingTriggerDeterminant().setJenkinsUserBuildVars(run, null, variables, taskListener));
         assertThat(variables, equalTo(Collections.emptyMap()));
-    }
-
-//    @Test
-//    public void setVarsReturnsTrueWithBuildUsersVarsOnValidCause() throws Exception {
-//        EnvVars variables = new EnvVars();
-//        TaskListener taskListener = new LogTaskListener(Logger.getLogger("test-logger"), Level.INFO);
-//        Run run = r.createFreeStyleProject().getBuild("0");
-//        assertTrue(new BranchIndexingTriggerDeterminant().setJenkinsUserBuildVars(run, mockCause(), variables, taskListener));
-//        assertThat(variables, allOf(hasEntry(IUsernameSettable.BUILD_USER_VAR_NAME, "Branch Indexing"),
-//                hasEntry(IUsernameSettable.BUILD_USER_FIRST_NAME_VAR_NAME, "Branch"),
-//                hasEntry(IUsernameSettable.BUILD_USER_LAST_NAME_VAR_NAME, "Indexing"),
-//                hasEntry(IUsernameSettable.BUILD_USER_ID, "branchIndexing")
-//        ));
-//    }
-
-    private BranchIndexingCause mockCause() throws Exception {
-        Constructor<BranchIndexingCause> ctor = BranchIndexingCause.class.getDeclaredConstructor();
-        ctor.setAccessible(true);
-        return ctor.newInstance();
     }
 }
